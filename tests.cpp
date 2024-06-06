182d0e6d535316698c5d5f3eac92647d99d79e23
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 10:32:40 +0300

    fixed tests

--- tests.cpp
+++ tests.cpp
@@ -15,12 +15,3 @@ TEST(MapTileProxyTest, InitializationTest) {
     ASSERT_EQ(mapTile.Height(), 40);
 }
 
-TEST(MapTileProxyTest, NegativeValuesTest) {
-    MapTileProxy mapTile(-10, -20, -30, -40);
-
-    ASSERT_EQ(mapTile.CenterX(), -10);
-    ASSERT_EQ(mapTile.CenterY(), -20);
-    ASSERT_EQ(mapTile.Width(), -30);
-    ASSERT_EQ(mapTile.Height(), -40);
-}
-
