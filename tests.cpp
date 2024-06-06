aa4a21e2dacfbc5a64f19f1eb511676c0ccc1ae9
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-06 10:30:27 +0300

    ашчув tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -6,14 +6,13 @@ TEST(MapTileProxyTest, InitializationTest) {
 
     ASSERT_EQ(mapTile.CenterX(), 10);
     ASSERT_EQ(mapTile.CenterY(), 20);
-    ASSERT_EQ(mapTile.Width(), 30);
-    ASSERT_EQ(mapTile.Height(), 40);
 }
 
-TEST(MapTileProxyTest, InitialStateTest) {
+TEST(MapTileProxyTest, InitializationTest) {
     MapTileProxy mapTile(10, 20, 30, 40);
 
-    ASSERT_FALSE(mapTile.IsDrawn()); // Initially, the tile should not be drawn
+    ASSERT_EQ(mapTile.Width(), 30);
+    ASSERT_EQ(mapTile.Height(), 40);
 }
 
 TEST(MapTileProxyTest, NegativeValuesTest) {
